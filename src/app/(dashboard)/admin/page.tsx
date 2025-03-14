399dba3704fd33f5fc89902139e9da870b347786
Chinweike99 <innocentakwolu1@gmail.com>
2025-03-14 05:19:35 -0700

    userCards

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,24 @@
+import UserCard from '@/app/components/UserCard'
 import React from 'react'
 
 const Adminpage = () => {
   return (
-    <div>page</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row mt-2'>
+      <div className='w-full lg:w-2/3'>
+
+        <div className='flex gap-4 justify-between flex-wrap'>
+        <UserCard type="student" />
+        <UserCard type="teacher"/>
+        <UserCard type="parent"/>
+        <UserCard type="staff"/>
+        </div>
+      </div>
+
+      <div className='w-full lg:w-1/3'>
+        Left
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/app/components/UserCard.tsx
@@ -0,0 +1,19 @@
+import { MoreHorizontal } from 'lucide-react'
+import React from 'react'
+
+const UserCard = ({type}: {type:string}) => {
+  return (
+    <div className='rounded-2xl odd:bg-gray-500 even:bg-blue-400 p-4 flex-1 min-w-[130px]'>
+      <div className='flex justify-between items-center'>
+        <span className='text-[10px] bg-gray-300 px-2 py-1 rounded-full '>
+            15/2025
+        </span>
+        <MoreHorizontal />
+      </div>
+      <h1 className='text-2xl font-semibold my-4'>2,000</h1>
+      <h2 className='capitalize text-sm font-medium text-gray-300'>{type}s</h2>
+    </div>
+  )
+}
+
+export default UserCard
