a1cf4ee573d2f912d1f72df4c3b7309b4f2f3776
Chinweike99 <innocentakwolu1@gmail.com>
2025-03-16 00:43:18 +0100

    users home page

--- src/app/components/Menubar.tsx
+++ src/app/components/Menubar.tsx
@@ -107,7 +107,7 @@ const menuItem = [
         {
             icon: <BookCopy />,
             label: "Teachers",
-          href: "teacher",
+          href: "/teacher",
           visible: ["admin", "teacher"],
         },
         {
