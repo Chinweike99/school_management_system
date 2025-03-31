fbbb5df64d45695593a27b6bfe7409e10e8ff860
Chinweike99 <innocentakwolu1@gmail.com>
2025-03-31 07:14:25 +0100

    updated

--- src/app/(dashboard)/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function DashboardPage() {
-    return (
-      <div>
-        <h1 className="text-2xl font-bold">Welcome to the Dashboard</h1>
-        <p className="mt-2">This is your main dashboard page.</p>
-      </div>
-    );
-  }
-  
